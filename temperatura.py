c53110465af74f6eb48f951badb9c9f51120d2f2
Peltier Diaz Ulloa <pdiazu@uamv.edu.ni>
2026-08-19 10:32:24 -0600

    Dejar solo ejercicio de operadores

--- temperatura.py
+++
@@ -1,8 +0,0 @@
-# Solicitar la temperatura en grados Celsius
-celsius = float(input("Digite la temperatura en grados Celsius: "))
-
-# Convertir la temperatura a grados Fahrenheit
-fahrenheit = (celsius * 9 / 5) + 32
-
-# Mostrar el resultado
-print(celsius, "grados Celsius equivalen a", fahrenheit, "grados Fahrenheit.")
