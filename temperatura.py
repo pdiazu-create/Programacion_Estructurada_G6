e5dbb8a7c87b30d4dafd1317d4b89993e52204cb
Peltier Diaz Ulloa <pdiazu@uamv.edu.ni>
2026-08-19 10:31:39 -0600

    Agregar ejercicios de Programacion Estructurada

---
+++ temperatura.py
@@ -0,0 +1,8 @@
+# Solicitar la temperatura en grados Celsius
+celsius = float(input("Digite la temperatura en grados Celsius: "))
+
+# Convertir la temperatura a grados Fahrenheit
+fahrenheit = (celsius * 9 / 5) + 32
+
+# Mostrar el resultado
+print(celsius, "grados Celsius equivalen a", fahrenheit, "grados Fahrenheit.")
