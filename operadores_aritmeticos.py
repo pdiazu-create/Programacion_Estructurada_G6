f555ec8dcadd593343d8965f4d819290ea993cda
Peltier Diaz Ulloa <pdiazu@uamv.edu.ni>
2026-08-19 10:43:10 -0600

    Agregar operadores aritmeticos

---
+++ operadores_aritmeticos.py
@@ -0,0 +1,10 @@
+# Operadores Aritmeticos suma, resta, multiplicacion, division, porcentaje.
+
+number1 = int(input("Ingresa un numero: "))
+number2 = int(input("Ingresa otro numero: "))
+
+print(f"{number1} + {number2} = {number1 + number2}")
+print(f"{number1} - {number2} = {number1 - number2}")
+print(f"{number1} * {number2} = {number1 * number2}")   
+print(f"{number1} / {number2} = {number1 / number2}")
+print(f"{number1} % {number2} = {number1 % number2}")
